5cd043f20df104502b22f3628753126699c860cf
Stone Werner <stonewerner@berkeley.edu>
2023-12-01 19:06:04 +0000

    still addressing rubocop, down to 2 offenses

--- app/controllers/candidate_controller.rb
+++ app/controllers/candidate_controller.rb
@@ -2,36 +2,57 @@
 
 class CandidateController < ApplicationController
   def search
-    @election_cycles = %w[2010 2012 2014 2016 2018 2020]
-    @financial_categories = %w[candidate-loan contribution-total debts-owed disbursements-total end-cash
-                               individual-total pac-total receipts-total refund-total]
-    if params[:cycle] && params[:category]
-      cycle = params[:cycle]
-      category = params[:category]
-      url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
-      output = Faraday.get(url) do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['X-API-Key'] = '9lcjslvwVjbqtX0KcQQ3W9rFm316caQQ2T89n4xA'
-      end
-      Rails.logger.info("Request URL: #{url}")
-      Rails.logger.info("Response: #{output.body}")
-      begin
-        response_data = JSON.parse(output.body)
-      rescue JSON::ParserError => e
-        Rails.logger.error("Error parsing JSON: #{e.message}")
-      end
-      if response_data.nil?
-        # If the request is not successful, handle the error
-        @error_message = response.body['message']
-        flash[:alert] = 'Error fetching from API. Try again'
-        render 'candidate/search'
-      else
-        # If the request is successful, call the model function
-        @candidates = Candidate.propublica_to_candidates(response_data)
-        @cycle = cycle
-        @category = category
-        render 'candidate/results'
-      end
+    return unless valid_params?
+
+    fetch_candidate_data
+  end
+
+  private
+
+  def valid_params?
+    params[:cycle].present? && params[:category].present?
+  end
+
+  def fetch_candidate_data
+    cycle = params[:cycle]
+    category = params[:category]
+    url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
+
+    output = fetch_data_from_api(url)
+
+    handle_response(output)
+  end
+
+  def fetch_data_from_api(url)
+    Faraday.get(url) do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['X-API-Key'] = '9lcjslvwVjbqtX0KcQQ3W9rFm316caQQ2T89n4xA'
     end
+  end
+
+  def handle_response(output)
+    Rails.logger.info("Request URL: #{output.env.url}")
+    Rails.logger.info("Response: #{output.body}")
+
+    begin
+      response_data = JSON.parse(output.body)
+      render_results(response_data)
+    rescue JSON::ParserError => e
+      Rails.logger.error("Error parsing JSON: #{e.message}")
+      handle_error
+    end
+  end
+
+  def render_results(response_data)
+    @candidates = Candidate.propublica_to_candidates(response_data)
+    @cycle = params[:cycle]
+    @category = params[:category]
+    render 'candidate/results'
+  end
+
+  def handle_error
+    @error_message = response.body['message']
+    flash[:alert] = 'Error fetching from API. Try again'
+    render 'candidate/search'
   end
 end
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -7,10 +7,14 @@ RSpec.describe Representative, type: :model do
     let(:rep_info) { instance_double(rep_info) }
 
     before do
-      allow(rep_info).to receive(:officials).and_return([instance_double(official, name: 'Stone Werner', party: 'Democrat',
-address: 'Los Angeles', photo_url: '')])
-      allow(rep_info).to receive(:offices).and_return([instance_double(office1, name: 'Mayor', division_id: 'ocdid1',
-official_indices: [0])])
+      allow(rep_info).to receive(:officials).and_return([
+                                                          instance_double(official, name: 'Stone Werner', party: 'Democrat',
+                                                          address: 'Los Angeles', photo_url: '')
+                                                        ])
+      allow(rep_info).to receive(:offices).and_return([
+                                                        instance_double(office1, name: 'Mayor', division_id: 'ocdid1',
+                                                        official_indices: [0])
+                                                      ])
     end
 
     it 'no matching name, creates new rep' do
